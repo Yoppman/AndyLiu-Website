ba9e5fcfa227b9f57db0c9a1c08718dad43c840c
LIU-CHIA-DA <itri461370@itri.org.tw>
2025-05-29 03:03:24 -0700

    Add joshua tree & palm spring collection

---
+++ src/data/galleries/PalmSpringGallery.ts
@@ -0,0 +1,36 @@
+import { Gallery } from '../types/galleries';
+
+export const PalmSpringGallery: Gallery = {
+  slug: 'palmspring',
+  title: 'Palm Spring',
+  description: 'Palm Spring',
+  photos: [
+    { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512413/palmspring/dsc04910.jpg', orientation: 'horizontal', dominantColor: 'rgba(121, 125, 116, 0.6)' },
+    { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512313/palmspring/dsc04832.jpg', orientation: 'horizontal', dominantColor: 'rgba(115, 108, 90, 0.6)' },
+    { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512363/palmspring/dsc04893.jpg', orientation: 'horizontal', dominantColor: 'rgba(145, 133, 115, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512352/palmspring/dsc04878.jpg', orientation: 'horizontal', dominantColor: 'rgba(181, 170, 139, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512322/palmspring/dsc04836.jpg', orientation: 'vertical', dominantColor: 'rgba(130, 118, 97, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512329/palmspring/dsc04853.jpg', orientation: 'horizontal', dominantColor: 'rgba(27, 26, 19, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512334/palmspring/dsc04863.jpg', orientation: 'horizontal', dominantColor: 'rgba(171, 148, 116, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512340/palmspring/dsc04873.jpg', orientation: 'vertical', dominantColor: 'rgba(76, 86, 81, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512345/palmspring/dsc04877.jpg', orientation: 'horizontal', dominantColor: 'rgba(34, 31, 26, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512369/palmspring/dsc04895.jpg', orientation: 'horizontal', dominantColor: 'rgba(206, 202, 186, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512374/palmspring/dsc04898.jpg', orientation: 'vertical', dominantColor: 'rgba(22, 21, 14, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512383/palmspring/dsc04899.jpg', orientation: 'horizontal', dominantColor: 'rgba(41, 37, 26, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512396/palmspring/dsc04900.jpg', orientation: 'vertical', dominantColor: 'rgba(198, 174, 144, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512401/palmspring/dsc04908.jpg', orientation: 'horizontal', dominantColor: 'rgba(57, 40, 24, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512406/palmspring/dsc04909.jpg', orientation: 'horizontal', dominantColor: 'rgba(75, 58, 38, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512418/palmspring/dsc04915.jpg', orientation: 'horizontal', dominantColor: 'rgba(225, 197, 157, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512426/palmspring/dsc05018.jpg', orientation: 'vertical', dominantColor: 'rgba(206, 219, 215, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512431/palmspring/dsc05072.jpg', orientation: 'vertical', dominantColor: 'rgba(58, 59, 22, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512437/palmspring/dsc05091.jpg', orientation: 'vertical', dominantColor: 'rgba(226, 218, 192, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512443/palmspring/dsc05095.jpg', orientation: 'vertical', dominantColor: 'rgba(198, 186, 151, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512452/palmspring/dsc05119.jpg', orientation: 'horizontal', dominantColor: 'rgba(40, 36, 17, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512457/palmspring/dsc05155.jpg', orientation: 'vertical', dominantColor: 'rgba(59, 46, 23, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512463/palmspring/dsc05163.jpg', orientation: 'vertical', dominantColor: 'rgba(36, 45, 37, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512468/palmspring/dsc05170.jpg', orientation: 'vertical', dominantColor: 'rgba(24, 44, 37, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1748512473/palmspring/dsc05171.jpg', orientation: 'vertical', dominantColor: 'rgba(26, 40, 27, 0.6)' }
+  ]
+};
+
+export default PalmSpringGallery;
