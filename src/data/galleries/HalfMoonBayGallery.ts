4a567b1b3a0bd0eb2998e99fe201a89c8a97476f
LIU-CHIA-DA <itri461370@itri.org.tw>
2025-07-19 01:17:12 -0700

    Add new collection

---
+++ src/data/galleries/HalfMoonBayGallery.ts
@@ -0,0 +1,48 @@
+import { Gallery } from '../types/galleries';
+
+export const HalfMoonBayGallery: Gallery = {
+  slug: 'halfmoonbay',
+  title: 'Halfmoonbay',
+  description: 'Halfmoonbay',
+  photos: [
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190431/halfmoonbay/dsc06163.jpg', orientation: 'horizontal', dominantColor: 'rgba(59, 106, 126, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190436/halfmoonbay/dsc06166.jpg', orientation: 'horizontal', dominantColor: 'rgba(46, 49, 29, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190450/halfmoonbay/dsc06169.jpg', orientation: 'vertical', dominantColor: 'rgba(209, 230, 238, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190457/halfmoonbay/dsc06172.jpg', orientation: 'vertical', dominantColor: 'rgba(26, 46, 28, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190464/halfmoonbay/dsc06174.jpg', orientation: 'horizontal', dominantColor: 'rgba(55, 67, 60, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190469/halfmoonbay/dsc06175.jpg', orientation: 'vertical', dominantColor: 'rgba(229, 228, 206, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190476/halfmoonbay/dsc06176.jpg', orientation: 'horizontal', dominantColor: 'rgba(218, 219, 201, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190484/halfmoonbay/dsc06179.jpg', orientation: 'horizontal', dominantColor: 'rgba(66, 68, 40, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190494/halfmoonbay/dsc06181.jpg', orientation: 'horizontal', dominantColor: 'rgba(200, 203, 185, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190501/halfmoonbay/dsc06193.jpg', orientation: 'horizontal', dominantColor: 'rgba(201, 210, 198, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190511/halfmoonbay/dsc06194.jpg', orientation: 'horizontal', dominantColor: 'rgba(194, 197, 164, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190517/halfmoonbay/dsc06195.jpg', orientation: 'vertical', dominantColor: 'rgba(147, 184, 190, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190522/halfmoonbay/dsc06220.jpg', orientation: 'horizontal', dominantColor: 'rgba(175, 174, 168, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190528/halfmoonbay/dsc06239.jpg', orientation: 'vertical', dominantColor: 'rgba(41, 45, 10, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190535/halfmoonbay/dsc06242.jpg', orientation: 'horizontal', dominantColor: 'rgba(66, 69, 20, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190543/halfmoonbay/dsc06252.jpg', orientation: 'horizontal', dominantColor: 'rgba(209, 209, 187, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190550/halfmoonbay/dsc06278.jpg', orientation: 'vertical', dominantColor: 'rgba(36, 46, 15, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190555/halfmoonbay/dsc06284.jpg', orientation: 'vertical', dominantColor: 'rgba(43, 46, 19, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190561/halfmoonbay/dsc06285.jpg', orientation: 'vertical', dominantColor: 'rgba(170, 199, 198, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190568/halfmoonbay/dsc06293.jpg', orientation: 'horizontal', dominantColor: 'rgba(82, 77, 49, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190575/halfmoonbay/dsc06294.jpg', orientation: 'vertical', dominantColor: 'rgba(70, 66, 44, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190581/halfmoonbay/dsc06300.jpg', orientation: 'vertical', dominantColor: 'rgba(186, 210, 207, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190585/halfmoonbay/dsc06319.jpg', orientation: 'horizontal', dominantColor: 'rgba(42, 31, 11, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190591/halfmoonbay/dsc06327.jpg', orientation: 'horizontal', dominantColor: 'rgba(81, 76, 44, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190597/halfmoonbay/dsc06328.jpg', orientation: 'horizontal', dominantColor: 'rgba(76, 67, 35, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190604/halfmoonbay/dsc06336.jpg', orientation: 'vertical', dominantColor: 'rgba(189, 207, 203, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190609/halfmoonbay/dsc06340.jpg', orientation: 'vertical', dominantColor: 'rgba(185, 208, 208, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190615/halfmoonbay/dsc06342.jpg', orientation: 'horizontal', dominantColor: 'rgba(194, 210, 204, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190623/halfmoonbay/dsc06345.jpg', orientation: 'vertical', dominantColor: 'rgba(51, 50, 37, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190628/halfmoonbay/dsc06346.jpg', orientation: 'vertical', dominantColor: 'rgba(226, 219, 201, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190634/halfmoonbay/dsc06354.jpg', orientation: 'vertical', dominantColor: 'rgba(67, 65, 49, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190639/halfmoonbay/dsc06355.jpg', orientation: 'horizontal', dominantColor: 'rgba(199, 199, 199, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190644/halfmoonbay/dsc06380.jpg', orientation: 'vertical', dominantColor: 'rgba(115, 104, 77, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190650/halfmoonbay/dsc06383.jpg', orientation: 'horizontal', dominantColor: 'rgba(69, 62, 42, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190655/halfmoonbay/dsc06442.jpg', orientation: 'horizontal', dominantColor: 'rgba(115, 102, 62, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190658/halfmoonbay/dsc06448.jpg', orientation: 'vertical', dominantColor: 'rgba(24, 20, 15, 0.6)' },
+      { src: 'https://res.cloudinary.com/dlfmzlwp6/image/upload/v1751190664/halfmoonbay/dsc06466.jpg', orientation: 'vertical', dominantColor: 'rgba(196, 209, 201, 0.6)' }
+  ]
+};
+
+export default HalfMoonBayGallery;
